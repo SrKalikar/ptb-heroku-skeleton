4a5dc54875057220acb84c68bc1d3b8aa4ef72ea
Shubham Kalikar <33619493+SrKalikar@users.noreply.github.com>
2021-07-06 19:56:49 +0530

    token_change
    token changed and file name given

--- herokubot.py
+++ herokubot.py
@@ -17,8 +17,8 @@ def error(bot, update, error):
 
 if __name__ == "__main__":
     # Set these variable to the appropriate values
-    TOKEN = "Your token from @Botfather"
-    NAME = "The name of your app on Heroku"
+    TOKEN = "1554223528:AAH7ZlltAlGrlCympw1V6P31ivBMEM2wrwU "
+    NAME = "ginger"
 
     # Port is given by Heroku
     PORT = os.environ.get('PORT')
